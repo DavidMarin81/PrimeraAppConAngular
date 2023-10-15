b8cb76f55ebb0c82cdbc92f0bcfcb14ee39af728
David Marin <davidmaringomez81@gmail.com>
2023-10-15 13:24:31 +0200

    Directivas ngIf - ngFor

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterComponent implements OnInit {
 
-  public counter: number = 1;
+  public counter: number = 0;
+
+  public heroNames: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Batman', 'Thor'];
   
   constructor() { }
 
@@ -16,13 +18,25 @@ export class CounterComponent implements OnInit {
 
   changeCounter(value: number): void {
     this.counter += value;
-    if(this.counter == 0){
-      this.counter = 1;
+    if(this.counter < 0){
+      this.counter = 0;
+    } else {
+      document.querySelector('h2')!.innerHTML = '<h2>Adding products</h2>';
     }
   }
 
   reset(): void {
-    this.counter = 1;
+    this.counter = 0;
+    document.querySelector('h2')!.innerHTML = '<h2>Any Products</h2>';
+  }
+
+  addHero(): void {
+    var superHeroName = prompt("Introduce un superheroe");
+    this.heroNames.push(superHeroName!);
+  }
+
+  removeLastHero(): void {
+      this.heroNames.pop();
   }
 
 }
